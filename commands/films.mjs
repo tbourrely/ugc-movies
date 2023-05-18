552819c94a74842eb2e3c3ff73895d23140d1c39
Thomas Bourrely <thomas.bourrely@protonmail.com>
2023-05-18 15:45:45 +0200

    move format function to theaters lib

--- commands/films.mjs
+++ commands/films.mjs
@@ -1,22 +1,8 @@
-import { SlashCommandBuilder, bold } from 'discord.js';
-import { theaters, getMovies } from '../lib/theaters.mjs';
+import { SlashCommandBuilder } from 'discord.js';
+import { theaters, getMovies, formatList } from '../lib/theaters.mjs';
 import { getTomorrowDate } from '../lib/date.mjs';
 
 const cinemaChoices = Object.keys(theaters).map(element => ({ name: element, value: theaters[element] }));
-
-const format = (movieList) => {
-	let output = '';
-
-	movieList.forEach(movie => {
-		const movieLine = bold(`${movie.film} - ${movie.gender}`);
-		const seances = movie.seances.reduce((acc, seance) => {
-			return acc + `* ${seance.hour} - ${seance.version}\n`;
-		}, '');
-		output += `\n${movieLine}\n${seances}`;
-	});
-
-	return output;
-};
 
 export default {
 	data: new SlashCommandBuilder()
@@ -31,7 +17,7 @@ export default {
 	async execute(interaction) {
 		const cinema = interaction.options.getNumber('cinema');
 		const rawMovieList = await getMovies(getTomorrowDate(), cinema);
-		const formatted = format(rawMovieList);
+		const formatted = formatList(rawMovieList);
 		await interaction.reply(formatted);
 		// FIXME: max length issue with part dieu
 	},
