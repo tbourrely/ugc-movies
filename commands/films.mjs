0ed91c3e0df7c867a938fd2ed6345c2fbc598186
Thomas Bourrely <thomas.bourrely@protonmail.com>
2023-05-18 23:20:44 +0200

    add start_after option to films command

--- commands/films.mjs
+++ commands/films.mjs
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { theaters, getMovies, formatList, splitFormattedList } from '../lib/theaters.mjs';
+import { theaters, getMovies, formatList, splitFormattedList, filterStartTimeAfter } from '../lib/theaters.mjs';
 import { getTomorrowDate, fromDayMonth } from '../lib/date.mjs';
+import { isValid } from '../lib/time.mjs';
 import { MAX_MESSAGE_LENGTH } from '../constants.js';
 
 const cinemaChoices = Object.keys(theaters).map(element => ({ name: element, value: theaters[element] }));
@@ -20,18 +21,29 @@ export default {
 				.setDescription('Date souhaitee (31/12)')
 				.setMinLength(5)
 				.setMaxLength(5),
+		)
+		.addStringOption(option =>
+			option.setName('start_after')
+				.setDescription('Heure de debut (18:30)')
+				.setMinLength(5)
+				.setMaxLength(5),
 		),
 	async execute(interaction) {
 		const cinemaInput = interaction.options.getNumber('cinema');
 		const dateInput = interaction.options.getString('date');
+		const startAfterInput = interaction.options.getString('start_after');
 
 		const date = dateInput ? fromDayMonth(dateInput) : getTomorrowDate();
 
-		const rawMovieList = await getMovies(date, cinemaInput);
+		let rawMovieList = await getMovies(date, cinemaInput);
 
 		if (rawMovieList.length == 0) {
 			await interaction.reply('Pas de films pour cette date (ou pas encore...)');
 			return;
+		}
+
+		if (startAfterInput && isValid(startAfterInput)) {
+			rawMovieList = filterStartTimeAfter(rawMovieList, startAfterInput);
 		}
 
 		const formatted = formatList(rawMovieList);
